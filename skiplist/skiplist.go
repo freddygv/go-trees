0ad38fe2c03404a96d3aa779e2d5ff0da893b04a
freddygv <vallenilla.fr@gmail.com>
2018-05-01 16:21:54 +0100

    Add Get method to Skip List

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -2,58 +2,111 @@ package skiplist
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
-// List contains a reference to the root of the list
+// List exports a reference to the head of the list
 type List struct {
-	Root *List
+	Head *Node
+	tail *Node
 	rnd  *rand.Rand
 }
 
-const maxHeight = 31
+const (
+	maxHeight = 31
+	minValue  = math.MinInt32
+	maxValue  = math.MaxInt32
+)
 
 // New returns an empty Skip List
 func New() *List {
+	root := &Node{
+		Value:     minValue,
+		Level:     maxHeight,
+		Shortcuts: make([]*Node, maxHeight),
+	}
+	tail := &Node{
+		Value:     maxValue,
+		Level:     maxHeight,
+		Shortcuts: make([]*Node, maxHeight),
+	}
 	return &List{
-		rnd: rand.New(rand.NewSource(time.Now().UnixNano())),
+		Head: root,
+		tail: tail,
+		rnd:  rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 }
 
-// Get searches the list for a value, returns node ptr and boolean indicating if found
-// TODO:
-func (tree *List) Get(value int) (*List, bool) {
-	return nil, false
+// Get searches the list for a target, returns node ptr and boolean indicating if found.
+// If the target value is not found, a pointer to the previous node is returned
+func (list *List) Get(target int) (*Node, bool) {
+	level := maxHeight
+	current := list.Head
+
+	for level >= 0 {
+		neighbor := current.Shortcuts[level]
+
+		// Advance while the neighbor does not overshoot target
+		for compare(neighbor.Value, target) <= 0 {
+			current = neighbor
+			neighbor = current.Shortcuts[level]
+		}
+
+		if compare(target, current.Value) == 0 {
+			return current, true
+		}
+		level--
+	}
+
+	return current, false
 }
 
 // Insert will add a new node to the list with the given value
-// TODO:
-func (tree *List) Insert(value int) {
+func (list *List) Insert(value int) {
+	var level int8
 
+	// Generate random int32 and take least significant bits as coinflips to increment level
+	r := list.rnd.Int31()
+	for r&1 == 1 {
+		level++
+		r >>= 1
+	}
+
+	// TODO: Update shortcuts
+	// previous, _ := list.Get(value)
+	// inserted := Node{
+	// 	Value:     value,
+	// 	Level:     level,
+	// 	Shortcuts: make([]*Node, maxHeight),
+	// }
 }
 
 // TODO:
-func (tree *List) toSlice() []*List {
-	arr := make([]*List, 0)
+func (list *List) toSlice() []*Node {
+	arr := make([]*Node, 0)
 
 	return arr
 }
 
-// Node is a sub-tree
+// Node is a sub-list
 type Node struct {
-	Value    int
-	Priority int
-	Left     *Node
-	Right    *Node
-	Parent   *Node
+	Value     interface{}
+	Level     int8
+	Shortcuts []*Node
 }
 
-// TODO:
 func (node *Node) String() string {
-	return fmt.Sprintf("%v", node.Value)
+	return fmt.Sprintf("%v,%v", node.Value, node.Level)
 }
 
-func compare(a, b int) int {
-	return a - b
+// TODO: Don't panic
+func compare(a, b interface{}) int {
+	intA, okA := a.(int)
+	intB, okB := b.(int)
+	if !okA || !okB {
+		panic("item is not an integer")
+	}
+	return intA - intB
 }
